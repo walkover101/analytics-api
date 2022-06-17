1031ff86aff46c2ca53f94d19e100584a9b4ee4c
Ankit Kumar <ankitkumar8492@gmail.com>
2022-06-18 00:23:39 +0530

    Fixed bq query

--- route/analytics.ts
+++ route/analytics.ts
@@ -5,13 +5,15 @@ const router = express.Router();
 router.route('/users/:userId')
     .get(async (req: Request, res: Response) => {
         const { userId, startDate = getDefaultDate().start, endDate = getDefaultDate().end } = req.params;
+        console.log(startDate);
+        console.log(endDate);
         const query = `SELECT DATE(sentTime) as Date, EXTRACT(HOUR FROM sentTime) as Hour,
         user_pid as Company, senderID as ID, SUM(credit) as Credit, 
         COUNTIF(status = 1) as Delivered, COUNTIF(status = 2) as Failed,
         COUNTIF(status = 1) + COUNTIF(status= 2) as Sent
         FROM \`msg91-reports.msg91_production.report_data\`
-        WHERE (sentTime BETWEEN ${startDate} AND ${endDate}) AND
-        user_pid = ${userId}
+        WHERE (sentTime BETWEEN "${endDate}" AND "${startDate}") AND
+        user_pid = "${userId}"
         GROUP BY DATE(sentTime), EXTRACT(HOUR FROM sentTime), user_pid, senderID;`
         const [job, stats] = await bigquery.createQueryJob({
             query,
@@ -20,7 +22,11 @@ router.route('/users/:userId')
         });
         console.log(getDefaultDate());
         const [rows] = await job.getQueryResults();
-        res.send(rows);
+        
+        res.send(rows.map(row=>{
+            row["Date"] = row["Date"].value;
+            return row;
+        }));
     });
 
 router.route('/users/:userId/campaigns/:campaignId')
