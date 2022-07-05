6e1bb591d52288b5cfd7488556e8a3a82cf858ab
Ankit Kumar <ankitkumar8492@gmail.com>
2022-07-05 09:49:10 +0530

    Improved sync script

---
+++ script.ts
@@ -0,0 +1,3 @@
+import dotenv from 'dotenv';
+import { main } from './sync/mongo-bquery';
+main();
--- sync/mongo-bquery.ts
+++ sync/mongo-bquery.ts
@@ -2,28 +2,31 @@ import { MongoClient } from 'mongodb';
 import fs from 'fs';
 import { DateTime } from 'luxon';
 import dotenv from 'dotenv';
+import bigQuery from '../database/big-query';
+const { dirname } = require('path');
+const appDir = dirname(require.main?.filename);
+const textReportSchema = ['_id', 'requestID', 'telNum', 'status', 'sentTime', 'providerSMSID', 'user_pid', 'senderID', 'smsc', 'deliveryTime', 'route', 'credit', 'retryCount', 'sentTimePeriod'];
 dotenv.config();
 const LAG = 48 * 60;  // Hours * Minutes
-const INTERVAL = 10   // Minutes 
+const INTERVAL = 5   // Minutes 
 // Connection URL
 const url = process.env.MONGO_CONNECTION_STRING || "";
 const client = new MongoClient(url);
-const timestampPointerFile = "../timestamp.txt";
-const lastDocumentProcessed = "../last-document.txt";
+const timestampPointerFile = `${appDir}/timestamp.txt`;
+const lastDocumentProcessed = `${appDir}/last-document.txt`;
 const dbName = process.env.MONGO_DB_NAME;
 
-async function main() {
+export async function main() {
     // console.log("Timestamp",getTimestamp());
     // Use connect method to connect to the server
     let connection = await client.connect();
     console.log('Connected successfully to server');
     const db = client.db(dbName);
     const collection = db.collection(process.env.MONGO_COLLECTION_NAME || "");
-    const prevDate = DateTime.fromISO("2022-01-29T11:11:08.224+00:00").toUTC().toUnixInteger();
     while (true) {
         try {
             // Read the timestamp from file and set it as startTime
-            var startTime = DateTime.fromISO(getLastTimestamp());
+            const startTime = DateTime.fromISO(getLastTimestamp());
             if (!startTime.isValid) {
                 throw new Error("Invalid startTime");
             }
@@ -37,7 +40,7 @@ async function main() {
             });
             console.log(`Time Limit : ${timeLimit}, End Time : ${endTime}, Diff : ${timeLimit.diff(endTime, 'minute').minutes}`)
             if (timeLimit.diff(endTime, 'minute').minutes <= 0) {
-                await dummyWait(5000);
+                await dummyWait((INTERVAL * 1000) / 4);
             } else {
                 console.log("Syncing Data...");
                 const { timestamp, documentId } = await syncData(collection, startTime, endTime, getLastDocument());
@@ -46,17 +49,17 @@ async function main() {
                 await dummyWait(1000);
             }
         } catch (error) {
-            console.log(error);
+            console.error(error);
             await dummyWait(10000);
         }
 
     }
 }
 
-main()
-    .then(console.log)
-    .catch(console.error)
-    .finally(() => client.close());
+// main()
+//     .then(console.log)
+//     .catch(console.error)
+//     .finally(() => client.close());
 
 async function syncData(collection: any, startTime: DateTime, endTime: DateTime, docuemntId?: string) {
     const output = {
@@ -65,23 +68,17 @@ async function syncData(collection: any, startTime: DateTime, endTime: DateTime,
     }
     const query = {
         sentTime: {
-            $gte: '',
-            $lt: ''
-        }
-    }
-    const apps = await collection.find({
-        updatedAt: {
             $gte: startTime,
             $lte: endTime
         }
-    }).sort({ updatedAt: 1 }).limit(2).toArray();
+    }
+    const docs = await collection.find(query).sort({ sentTime: 1 }).toArray();
     // console.log(apps);
     let skip = !!docuemntId;
-    for (let i = 0; i < apps.length; i++) {
-        const app = apps[i];
+    for (let i = 0; i < docs.length; i++) {
+        const app = docs[i];
         // Skip documents that have already been processed
         if (skip) {
-            console.log("Skiping", app);
             if (app._id == docuemntId) {
                 skip = false;
             } else {
@@ -89,12 +86,8 @@ async function syncData(collection: any, startTime: DateTime, endTime: DateTime,
             }
             continue;
         }
-        // TODO: ANKIT
-        // Add data to big query
-        // Handle main errors
-        // Break the loop if something is wrong with network
-        // Continue if error occurred because of data was wrong and push that data to a file
 
+        await insertRow("msg91_test", "text_report", [trimData(textReportSchema, app)]);
         // Update the pointer to the last processed document
         output.timestamp = DateTime.fromJSDate(app.updatedAt);
         if (!output.timestamp?.isValid) {
@@ -118,6 +111,7 @@ function getLastTimestamp() {
         let data = fs.readFileSync(timestampPointerFile, 'utf-8');
         return data.trim();
     } catch (error) {
+        console.error(error);
         throw new Error("Please set the initial timestamp to sync data from in timestamp.txt file");
     }
 }
@@ -146,4 +140,22 @@ function dummyWait(timeInMS: number) {
 }
 function getCurrentTimeInUTC() {
     return DateTime.now().toUTC().toUnixInteger();
+}
+
+async function insertRow(datasetId: string, tableId: string, rows: Array<Object>) {
+    try {
+        await bigQuery.dataset(datasetId).table(tableId).insert(rows);
+        console.log(`Inserted ${rows.length} rows`);
+    } catch (error) {
+        console.log(rows);
+        console.error(JSON.stringify(error));
+    }
+}
+
+function trimData(schema: Array<string>, data: { [key: string]: any }) {
+    let output: { [key: string]: any } = {};
+    schema.forEach((key: string) => {
+        output[key as string] = data[key as string];
+    })
+    return output;
 }
