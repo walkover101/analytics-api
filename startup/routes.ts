4a6d966fef9a542a0ad8225e8f46b29aec5bf961
Hatim Nagdawala <hatim@walkover.in>
2022-07-18 12:47:07 +0530

    Refactor: Remove unwanted route

--- startup/routes.ts
+++ startup/routes.ts
@@ -2,7 +2,6 @@ import express, { Application } from "express";
 import home from "../routes/home";
 import analytics from "../routes/analytics";
 import exportReports from "../routes/export-reports";
-import reports from "../routes/reports";
 import profits from '../routes/profits';
 import options from "../middlewares/options";
 import error from "../middlewares/error";
@@ -12,7 +11,6 @@ export default function (app: Application) {
   app.use(express.urlencoded({ extended: true }));
   app.use("/", home);
   app.use("/analytics", analytics);
-  app.use("/reports", reports);
   app.use("/exports", exportReports);
   app.use("/profits", profits);
   app.use(options);
