a259314384ff4f73a09e8476bb86f9b97e477b5f
Hatim Nagdawala <hatim@walkover.in>
2022-07-26 11:23:36 +0530

    Feat: Mail creation time in dlr-log-details

--- models/dlr-log-detail.model.ts
+++ models/dlr-log-detail.model.ts
@@ -3,11 +3,13 @@ export default class DlrLogDetail {
     eventId: number;
     uid: string;
     createdAt: Date;
+    mailCreationTime: Date;
 
     constructor(attr: any) {
         this.id = attr['_id'];
         this.eventId = parseInt(attr['eid']);
         this.uid = attr['uid'];
+        this.mailCreationTime = attr['mct']?.$date?.$numberLong && new Date(parseFloat(attr['mct']?.$date?.$numberLong));
         this.createdAt = attr['cat']?.$date?.$numberLong && new Date(parseFloat(attr['cat']?.$date?.$numberLong));
     }
 }
