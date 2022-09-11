013f14a12028d30ca5bc00f90ab0b1ea649aaabf
Ankit Kumar <ankitkumar@whozzat.com>
2022-09-11 23:05:44 +0530

    Removed avgDeliveryTime

--- services/sms/sms-analytics-service.ts
+++ services/sms/sms-analytics-service.ts
@@ -78,30 +78,26 @@ class SmsAnalyticsService {
             COUNTIF(reportData.status in (25, 16)) as rejected,
             COUNTIF(reportData.status = 9) as ndnc,
             COUNTIF(reportData.status = 17) as blocked,
-            COUNTIF(reportData.status = 7) as autoFailed,
-            ROUND(SUM(IF(reportData.status = 1, TIMESTAMP_DIFF(reportData.deliveryTime, reportData.sentTime, SECOND), NULL))/COUNTIF(reportData.status = 1), 0) as deliveryTime`;
+            COUNTIF(reportData.status = 7) as autoFailed`;
     }
 
     private calculateTotalAggr(data: any) {
-        let totalDeliveryTime = 0;
         const total = {
             "message": 0,
             "delivered": 0,
             "totalCredits": 0,
-            "filtered": 0,
-            "avgDeliveryTime": 0
+            "filtered": 0
         }
 
         data.forEach((row: any) => {
             total["message"] += row["sent"] || 0;
             total["delivered"] += row["delivered"] || 0;
             total["totalCredits"] += row["balanceDeducted"] || 0;
-            totalDeliveryTime += row["deliveryTime"] || 0;
         })
 
         total["filtered"] = total["message"] - total["delivered"];
         total["totalCredits"] = Number(total["totalCredits"].toFixed(3));
-        total["avgDeliveryTime"] = Number((totalDeliveryTime / data.length).toFixed(3));
+        
         return total;
     }
 }
