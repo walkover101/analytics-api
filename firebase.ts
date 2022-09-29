fd476d47379724abdca4efae5cfa868f9b3f0846
Ankit Kumar <ankitkumar@whozzat.com>
2022-09-29 10:27:21 +0530

    Moved Firebase Config To ENV

--- firebase.ts
+++ firebase.ts
@@ -1,11 +1,14 @@
-import { cert, initializeApp } from "firebase-admin/app";
+import { cert, initializeApp, ServiceAccount } from "firebase-admin/app";
 import { Firestore, getFirestore } from "firebase-admin/firestore";
 import admin from 'firebase-admin';
 // import service account file (helps to know the firebase project details)
-const serviceAccountCreds = require(process.env.FIREBASE_CREDENTIALS_FILE_PATH || './firebase-credentials.json');
-
+const serviceAccount: ServiceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+}
 admin.initializeApp({
-  credential: cert(serviceAccountCreds),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DB_URL
 });
 
