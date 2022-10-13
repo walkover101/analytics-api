67b84299df80e46dcda7760aa7c383d0e2cf1f1c
Ankit Kumar <ankitkumar@whozzat.com>
2022-10-13 08:31:59 +0530

    Converted data-integrity into a cron job

--- jobs/index.ts
+++ jobs/index.ts
@@ -3,8 +3,10 @@ import '../startup/string.extensions';
 import logger from "./../logger/logger";
 import { has } from 'lodash';
 import { requestDataSyncJob, reportDataSyncJob, otpReportSyncJob } from './sync-job';
+import { dataIntegrity } from "./data-integrity";
 import sequelize from '../database/sequelize-service';
-const argv = require('minimist')(process.argv.slice(2));
+import cron from 'node-cron';
+const args = require('minimist')(process.argv.slice(2));
 
 // Register your jobs here
 const Jobs: any = {
@@ -13,24 +15,42 @@ const Jobs: any = {
     otpReportSyncJob
 };
 
-function invalidJobName(jobName: string): Boolean {
-    return !has(Jobs, jobName)
+// Register your cron jobs
+const CronJobs: any = {
+    dataIntegrity
+};
+
+
+async function main() {
+    let jobs = Jobs;
+    await sequelize();
+    const jobName: string = args['name'] || args['n'];
+    const jobFrequency: string = args['frequency'] || args['freq'] || args['f'];
+    if (jobFrequency) {
+        if (!cron.validate(jobFrequency)) throw new Error("Please enter a valid frequency in quotes i.e '* * * * *'")
+        jobs = CronJobs;
+    }
+    if (isValidName(jobs, jobName)) throw new Error(`Valid job name is required\n\nAvailable jobs: \n${getAvailableJobs(jobs)}`);
+
+    // Run/Schedule the job
+    if (jobFrequency) {
+        const task = cron.schedule(jobFrequency, Jobs[jobName], { scheduled: false });
+        task.start();
+    } else {
+        jobs[jobName](args);
+    }
 }
 
-function getAvailableJobs() {
-    const jobNames = Object.keys(Jobs);
+
+
+
+function isValidName(jobs: any, jobName: string): Boolean {
+    return !has(jobs, jobName)
+}
+
+function getAvailableJobs(jobs: any) {
+    const jobNames = Object.keys(jobs);
     const numberedJobNames = jobNames.map((job, idx) => `${idx + 1}. ${job} -- --lts={LAST_TIMESTAMP}`);
     return numberedJobNames.join('\n');
 }
-
-async function main() {
-    await sequelize();
-    const jobName = process.argv[2];
-
-    if (invalidJobName(jobName))
-        return logger.error(`Valid job name is required\n\nAvailable jobs: \n${getAvailableJobs()}`);
-
-    Jobs[jobName](argv);
-}
-
 main();
