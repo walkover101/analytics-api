122bc801d36865dade83d2f84e7ef1d91282147d
Hatim Nagdawala <hatim@walkover.in>
2022-12-15 16:54:07 +0530

    Refactor: MailReport Model

--- database/big-query-service.ts
+++ database/big-query-service.ts
@@ -13,11 +13,9 @@ export const MAIL_REP_TABLE_ID = process.env.MAIL_REP_TABLE_ID || 'mail_report';
 export const MAIL_EVENTS_TABLE_ID = process.env.MAIL_EVENTS_TABLE_ID || 'mail_event';
 export const WA_REQ_TABLE_ID = process.env.WA_REQ_TABLE_ID || 'wa_request';
 export const WA_REP_TABLE_ID = process.env.WA_REP_TABLE_ID || 'wa_report';
-export const parent = `projects/${MSG91_PROJECT_ID}/datasets/${MSG91_DATASET_ID}/tables/${MAIL_REP_TABLE_ID}`;
-export const mode = require('@google-cloud/bigquery-storage').protos.google.cloud
-.bigquery.storage.v1.WriteStream.Type;
+export const mode = require('@google-cloud/bigquery-storage').protos.google.cloud.bigquery.storage.v1.WriteStream.Type;
 export const type = require('@google-cloud/bigquery-storage').protos.google.protobuf
-.FieldDescriptorProto.Type;
+    .FieldDescriptorProto.Type;
 
 const CREDENTIALS = {
     "private_key": process.env.PRIVATE_KEY,
@@ -32,7 +30,7 @@ const bigQuery = new BigQuery({
 
 const writeClient = new BigQueryWriteClient({
     credentials: CREDENTIALS,
-  });
+});
 
 function getMsg91Dataset() {
     return bigQuery.dataset(MSG91_DATASET_ID);
@@ -44,9 +42,24 @@ async function getQueryResults(query: string) {
     return rows;
 }
 
+async function getStream(parent: string, writeStream: { type: any }) {
+    const writeStreamName = await getWriteStreamName(parent, writeStream);
+    // This header is required so that BQ storage API knows which region to route the request to
+    const options = { otherArgs: { headers: { 'x-goog-request-params': `write_stream=${writeStreamName}` } } }
+    return await writeClient.appendRows(options);
+}
+
+async function getWriteStreamName(parent: string, writeStream: { type: any }) {
+    let request: any = { parent, writeStream };
+    let [response] = await writeClient.createWriteStream(request);
+    console.log(`Stream created: ${response.name}`);
+    return response.name;
+}
+
 export default getMsg91Dataset();
 export {
     bigQuery,
     getQueryResults,
-    writeClient
+    writeClient,
+    getStream
 }
--- models/mail-report.model.ts
+++ models/mail-report.model.ts
@@ -1,110 +1,94 @@
-import  { mode, parent, writeClient } from '../database/big-query-service';
+import { getStream, MSG91_PROJECT_ID, writeClient, MSG91_DATASET_ID, MAIL_REP_TABLE_ID, mode } from '../database/big-query-service';
 import { getHashCode } from "../services/utility-service";
 import protoDescriptor from './protofiles/mailReport_descriptor';
 import { DateTime } from 'luxon';
+
+const parent = `projects/${MSG91_PROJECT_ID}/datasets/${MSG91_DATASET_ID}/tables/${MAIL_REP_TABLE_ID}`;
+const writeStream = { type: mode.PENDING };
 export default class MailReport {
-    requestId: string; //Request Id of this mail (Not unique in this table)
-    eventId: number; //Events that occurred while processing this mail. (2,3,4,8,9)	
-    statusCode: number; //Standard Response Code of SMTP Protocol	
-    enhancedStatusCode: string; //Standard Response Code of SMTP Protocol	
-    reason: string; //Description of Response Status	
-    resultState: string;
-    remoteMX: string;
-    remoteIP: string; //MX Server IP of Client	
-    contentSize: number;
-    senderDedicatedIPId: number; //Unique id of Dedicated IP used to send this mail.
-    hostname: string;
-    recipientEmail: string; //Recipient Email Address
-    outboundEmailId: number; //Unique Id is generated for each request (All recipients, cc, bcc of that mail will have same id) in MySQL.
-    companyId: string; //Id of the company which requested this mail.
-    requestTime: string; //Timestamp when this mail was requested to be sent.	
-    createdAt: string; //Time when this specific event happened
+  requestId: string; //Request Id of this mail (Not unique in this table)
+  eventId: number; //Events that occurred while processing this mail. (2,3,4,8,9)	
+  statusCode: number; //Standard Response Code of SMTP Protocol	
+  enhancedStatusCode: string; //Standard Response Code of SMTP Protocol	
+  reason: string; //Description of Response Status	
+  resultState: string;
+  remoteMX: string;
+  remoteIP: string; //MX Server IP of Client	
+  contentSize: number;
+  senderDedicatedIPId: number; //Unique id of Dedicated IP used to send this mail.
+  hostname: string;
+  recipientEmail: string; //Recipient Email Address
+  outboundEmailId: number; //Unique Id is generated for each request (All recipients, cc, bcc of that mail will have same id) in MySQL.
+  companyId: string; //Id of the company which requested this mail.
+  requestTime: string; //Timestamp when this mail was requested to be sent.	
+  createdAt: string; //Time when this specific event happened
 
-    constructor(attr: any) {
-        this.eventId = parseInt(attr['eid']);
-        this.statusCode = parseInt(attr['stc']);
-        this.enhancedStatusCode = attr['esc'];
-        this.reason = attr['rsn'];
-        this.resultState = attr['rst'];
-        this.remoteMX = attr['rmx'];
-        this.remoteIP = attr['rip'];
-        this.contentSize = parseInt(attr['csz']);
-        this.senderDedicatedIPId = parseInt(attr['sid']);
-        this.hostname = attr['hnm'];
+  constructor(attr: any) {
+    this.eventId = parseInt(attr['eid']);
+    this.statusCode = parseInt(attr['stc']);
+    this.enhancedStatusCode = attr['esc'];
+    this.reason = attr['rsn'];
+    this.resultState = attr['rst'];
+    this.remoteMX = attr['rmx'];
+    this.remoteIP = attr['rip'];
+    this.contentSize = parseInt(attr['csz']);
+    this.senderDedicatedIPId = parseInt(attr['sid']);
+    this.hostname = attr['hnm'];
 
-        //common in all three email models
-        this.recipientEmail = attr['rem']?.toLowerCase();
-        this.outboundEmailId = parseInt(attr['oid']);
-        this.requestId = getHashCode(`${attr['mri']}-${this.recipientEmail}`);
-        this.companyId = attr['cid'];
-        this.requestTime = DateTime.fromJSDate(new Date(attr['mct'] || null)).toString();
-        this.createdAt = DateTime.fromJSDate(new Date(attr['created_at'] || null)).toString();
-    }
+    //common in all three email models
+    this.recipientEmail = attr['rem']?.toLowerCase();
+    this.outboundEmailId = parseInt(attr['oid']);
+    this.requestId = getHashCode(`${attr['mri']}-${this.recipientEmail}`);
+    this.companyId = attr['cid'];
+    this.requestTime = DateTime.fromJSDate(new Date(attr['mct'] || null)).toString();
+    this.createdAt = DateTime.fromJSDate(new Date(attr['created_at'] || null)).toString();
+  }
 
-    public static async insertMany(mailReports: Array<MailReport>) {
-      try {
-        let writeStream = {type: mode.PENDING};
-        let request: any = {
+  public static async insertMany(mailReports: Array<MailReport>) {
+    try {
+      const stream = await getStream(parent, writeStream);
+      const responses: any = [];
+
+      stream.on('data', (response: { error: { message: string | undefined; }; }) => {
+        if (response.error) {
+          throw new Error(response.error.message);
+        }
+
+        responses.push(response);
+        if (responses.length === mailReports.length) stream.end();
+      });
+
+      stream.on('error', (err: any) => {
+        throw err;
+      });
+
+      stream.on('end', async () => {
+        try {
+          let [response] = await writeClient.finalizeWriteStream({ // after this unable to append any data
+            name: writeStream,
+          });
+
+          console.info(`Row count: ${response.rowCount}`);
+
+          [response] = await writeClient.batchCommitWriteStreams({ // after this data will be available to read
             parent,
-            writeStream,
-        };
-        let [response] = await writeClient.createWriteStream(request); //stream is created
-        console.log(`Stream created: ${response.name}`);
+            writeStreams: [writeStream],
+          });
 
-        writeStream = response.name;
-        const options: {otherArgs: any} = {otherArgs: { headers: []}};
-        options.otherArgs.headers[
-            'x-goog-request-params'
-        ] = `write_stream=${writeStream}`;  // This header is required so that BQ storage API knows which region to route the request to
+          console.info(response);
+        } catch (err) {
+          console.error(err);
+        }
+      });
 
-        const stream = await writeClient.appendRows(options); 
-        const responses: any = [];
+      let protoRows: Object = {
+        writerSchema: { protoDescriptor },
+        rows: { serializedRows: mailReports },
+      };
 
-        stream.on('data', (response: { error: { message: string | undefined; }; }) => {
-            if (response.error) {
-              throw new Error(response.error.message);
-            }
-            console.log(response);
-            responses.push(response);
-            if (responses.length) {
-              stream.end();
-            }
-        });
-
-        stream.on('error', (err: any) => {
-          throw err;
-        });
-
-        stream.on('end', async () => {
-          try {
-            [response] = await writeClient.finalizeWriteStream({ // after this unable to append any data
-              name: writeStream,
-            });
-            console.log(`Row count: ${response.rowCount}`);
-  
-            [response] = await writeClient.batchCommitWriteStreams({ // after this data will be available to read
-              parent,
-              writeStreams: [writeStream],
-            });
-            console.log(response);
-          } catch (err) {
-            console.log(err);
-          }
-        });
-
-        let protoRows: Object = {
-          writerSchema: {protoDescriptor},
-          rows: {serializedRows: mailReports},
-        };
-
-        request = {
-          writeStream,
-          protoRows,
-        };
-        stream.write(request); //send batch
-    
-      } catch (error) {
-        console.log(error);
-      }
+      stream.write({ writeStream, protoRows }); //send batch
+    } catch (error) {
+      console.error(error);
     }
+  }
 }
