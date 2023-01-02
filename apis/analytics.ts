4d1e97685d735bea637e4d2ca0822026db34b5f8
Ankit Kumar <ankitkumar@whozzat.com>
2023-01-02 11:51:05 +0530

    Voice Apis

--- apis/analytics.ts
+++ apis/analytics.ts
@@ -9,13 +9,13 @@ const router = express.Router();
 router.get('/campaigns', analyticsController.getCampaignAnalytics);
 
 // GET '/analytics/sms' | '/analytics/mail' | '/analytics/otp' | '/analytics/wa'
-router.get(/^\/(sms|otp|mail|wa)/, analyticsController.getAnalytics);
+router.get(/^\/(sms|otp|mail|wa|voice)/, analyticsController.getAnalytics);
 
 // POST '/analytics/sms/export' | '/analytics/mail/export' | '/analytics/otp/export' | '/analytics/wa/export'
 router.post(/^\/(sms|mail|otp|wa)\/export\b/, downloadsController.downloadCsv(REPORT_TYPE.ANALYTICS));
 
 // GET '/analytics/sms/export' | '/analytics/mail/export' | '/analytics/otp/export' | '/analytics/wa/export'
-router.get(/^\/(sms|mail|otp|wa)\/export\b/, downloadsController.getDownloadLinks(REPORT_TYPE.ANALYTICS));
+router.get(/^\/(sms|mail|otp|wa|voice)\/export\b/, downloadsController.getDownloadLinks(REPORT_TYPE.ANALYTICS));
 
 // GET '/analytics/export' | '/analytics/export'
 router.get('/export', downloadsController.getDownloadLinks(REPORT_TYPE.ANALYTICS));
