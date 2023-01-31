3b5a199e41bc1d07129e0f52acaae45e5ccd1011
Vikas Ekka <walk-comp-038@makki.local>
2023-01-31 12:04:57 +0530

    fix: voice export logs

--- apis/downloads.ts
+++ apis/downloads.ts
@@ -5,9 +5,9 @@ import { REPORT_TYPE } from '../models/download.model';
 const router = express.Router();
 
 // POST '/exports/sms' | '/exports/otp' | '/exports/mail' | '/exports/wa'
-router.post(/^\/(sms|otp|mail|wa)/, downloadsController.downloadCsv(REPORT_TYPE.LOGS));
+router.post(/^\/(sms|otp|mail|wa|voice)/, downloadsController.downloadCsv(REPORT_TYPE.LOGS));
 
 // GET '/exports' | '/exports/sms' | '/exports/otp' | '/exports/mail' | '/exports/wa'
-router.get(/^\/($|sms|otp|mail|wa)/, downloadsController.getDownloadLinks(REPORT_TYPE.LOGS));
+router.get(/^\/($|sms|otp|mail|wa|voice)/, downloadsController.getDownloadLinks(REPORT_TYPE.LOGS));
 
 export default router;
